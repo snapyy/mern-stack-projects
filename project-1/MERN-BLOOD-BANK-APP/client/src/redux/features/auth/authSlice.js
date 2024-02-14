48246859239fd44da238bc547d93756bebe9f960
snapyy <210510113165@paruluniversity.ac.in>
2024-02-14 20:55:08 +0530

    Add files via upload

---
+++ project-1/MERN-BLOOD-BANK-APP/client/src/redux/features/auth/authSlice.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { getCurrentUser, userLogin, userRegister } from "./authAction";
+
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+// Initial state should be a plain object, not a function
+const initialState = {
+  loading: false,
+  user: null,
+  token,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState, // Provide the initial state object here
+  reducers: {},
+  extraReducers: (builder) => {
+    //login user
+    builder.addCase(userLogin.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    //the data which is going to return is called payload
+    builder.addCase(userLogin.fulfilled, (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.token;
+    });
+    builder.addCase(userLogin.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+
+    //REGISTER user
+    builder.addCase(userRegister.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+
+    builder.addCase(userRegister.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(userRegister.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+
+    //  CURRENT user
+    builder.addCase(getCurrentUser.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+
+    builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user;
+    });
+    builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    });
+  },
+});
+
+export default authSlice.reducer;
